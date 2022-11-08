6eaa64b67b6111d90453377f310edc60248ab5ea
Korolev <romakorolev9702@gmail.com>
2022-11-08 22:07:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/list/WorkersAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.list
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.list.databinding.WorkersItemBinding
+
+class WorkersAdapter:RecyclerView.Adapter<WorkersAdapter.WorkersHolder> {
+    
+
+}
+
+
